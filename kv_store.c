754257df59f6c3e80c5a03be47871e2dd9aa2412
Nathaniel Jester <njester@royal-10.cs.wisc.edu>
2024-04-16 13:58:15 -0500

    added ring_buffer.c code (hopefully working)

--- kv_store.c
+++ kv_store.c
@@ -1,11 +1,38 @@
-int put(key_type k, value_type v) {
+#include "common.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+struct entry {
+	key_type key;
+	value_type value;
+};
+
+struct kvstore {
+	struct entry* entries;
+	int max_size;
+	int size;
+};
+
+int put(struct kvstore kvstore, key_type k, value_type v) {
 	
 }
 
-int get(key_type k) {
+int get(struct kvstore kvstore, key_type k) {
 	
 }
 
-int main() {
-	
+int main(int argc, char* argv[]) {
+	struct kvstore* kvstore = malloc(sizeof(struct kvstore));
+	if (kvstore == NULL) {
+		printf("failed kvstore malloc");
+		return -1;
+	}
+	kvstore->max_size = (int) argv[4];
+	kvstore->size = 0;
+	kvstore->entries = calloc(kvstore->max_size, sizeof(struct entry));
+	if (kvstore->entries == NULL) {
+		free(kvstore);
+		printf("failed kvstore entries malloc");
+		return -1;
+	}
 }
--- ring_buffer.c
+++ ring_buffer.c
@@ -1,26 +1,64 @@
 #include "ring_buffer.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdatomic.h>
+#include <stdio.h>
 
 int init_ring(struct ring *r) {
+	if (r == NULL) {
+		return -1;
+	}
 	r->p_head = 0;
 	r->c_head = 0;
 	r->p_tail = 0;
 	r->c_tail = 0;
+	return 0;
 }
 
 void ring_submit(struct ring *r, struct buffer_descriptor *bd) {
-	uint32_t p_head = r->p_head;
-	uint32_t c_tail = r->c_tail;
-	if (c_tail > RING_SIZE) {
-		//block (don't remember how to do yet)
+	if (r == NULL || bd == NULL) {
+		return;
 	}
-	uint32_t p_next = p_head + 1;
-	r->p_head = p_next;
-	r->buffer[p_head] = *bd;
-	r->p_tail = r->p_head;
-	
-	
+	uint32_t c_tail;
+	uint32_t p_head;
+	while (1) {
+		c_tail = r->c_tail;
+		p_head = r->p_head;
+		while (p_head >= c_tail) {
+		}
+		if (atomic_compare_exchange_strong(&r->p_head, &p_head, p_head + 1)) {
+			break;
+		}
+	}
+	memcpy(&r->buffer[p_head % RING_SIZE], bd, sizeof(*bd));
+	while (1) {
+		if (r->p_tail == p_head) {
+			r->p_tail = r->p_tail + 1;
+			break;
+		}
+	}	
 }
 
 void ring_get(struct ring *r, struct buffer_descriptor *bd) {
-	
+	if (r == NULL || bd == NULL) {
+		return;
+	}
+	uint32_t p_tail;
+	uint32_t c_head;
+	while (1) {
+		c_head = r->c_head;
+		p_tail = r->p_tail;
+		while (c_head == p_tail) {
+		}
+		if (atomic_compare_exchange_strong(&r->c_head, &c_head, c_head + 1)) {
+			break;
+		}
+	}
+	memcpy(bd, &r->buffer[c_head % RING_SIZE], sizeof(*bd));
+	while (1) {
+		if(r->c_tail == c_head) {
+			r->c_tail = r->c_tail + 1;
+			break;
+		}
+	}
 }
