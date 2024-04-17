a494d02d76117141d76ec30109fc23f5c2d23088
Morgan Jaenke <jaenke@snares-03.cs.wisc.edu>
2024-04-16 19:38:50 -0500

    fixed some stuff in kv_store

--- kv_store.c
+++ kv_store.c
@@ -15,9 +15,9 @@ typedef struct HashNode {
 	struct HashNode * next;
 } HashNode_t;
 
-HashNode_t* hashtable[128];
-pthread_mutex_t mutex[128];
-int hash_size = 4;
+HashNode_t* hashtable[1000];
+pthread_mutex_t mutex[1000];
+int hash_size = 10;
 
 struct thread_context {
 	int tid;
@@ -30,6 +30,7 @@ int num_threads = 4;
 
 struct ring *ring = NULL;
 char * shmem_area = NULL;
+char shm_file[] = "shmem_file";
 
 /*
  * Puts key/value pair in the hashtable
@@ -38,26 +39,34 @@ int put(key_type k, value_type v) {
 	
 	int index = hash_function(k, hash_size);
 	
-	HashNode_t * newNode = (HashNode_t *) malloc(sizeof(HashNode_t));
-	newNode->k = k;
-	newNode->v = v;
-	newNode->next = NULL;
-	
 	pthread_mutex_lock(&mutex[index]);
 	
-	if (hashtable[index] == NULL) {
-		hashtable[index] = newNode;
-	} else {
-		HashNode_t * temp = hashtable[index];
+	HashNode_t * temp = hashtable[index];
+	if(hashtable[index] != NULL) {
 		while (temp->next != NULL && temp->k != k) {
 			temp = temp->next;
 		}
 		if (temp->k == k) {
 			temp->v = v;
-		} else {
-			temp->next = newNode;
 		}
+	} 
+	
+	HashNode_t * newNode = (HashNode_t * ) malloc(sizeof(HashNode_t));
+	newNode->k = k;
+	newNode->v = v;
+	newNode->next = NULL;
+	
+	if (hashtable[index] == NULL) {
+		hashtable[index] = newNode;
+	} else {
+		HashNode_t * temp = hashtable[index];
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+			temp->next = newNode;
+		
 	}
+	
 	pthread_mutex_unlock(&mutex[index]);
 	
 	return 0;
@@ -89,9 +98,9 @@ int get(key_type k) {
  * | RING | TID_0_COMPLETIONS | TID_1_COMPLETIONS | ... | TID_N_COMPLETIONS |
 */
 int init_server() {
-
-       int fd = shm_open("shmem_file", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-        
+	   printf("init server start\n");
+	   
+       int fd = open(shm_file, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
         if (fd < 0) {
         	perror("open");
 		}
@@ -123,7 +132,7 @@ int init_server() {
 */
 static int parse_args(int argc, char **argv) {
 	int op;
-	while ((op = getopt(argc, argv, "n:s:")) != -1) {
+	while ((op = getopt(argc, argv, "n:s:v")) != -1) {
 		switch (op) {
 			
 	        case 'n':
@@ -147,7 +156,7 @@ void serve_request(struct buffer_descriptor* bd) {
 		put(bd->k, bd->v);
 		bd->ready = 1;
 		memcpy(shmem_area + bd->res_off, bd, sizeof(*bd));
-	}
+	} 
 	if (bd->req_type == GET) {		
 		int value = get(bd->k);
 		struct buffer_descriptor *newbd;
@@ -164,8 +173,10 @@ void serve_request(struct buffer_descriptor* bd) {
  * @param arg context for this thread
 */
 void *thread_function(void *arg) {
+	printf("h\n");
     struct thread_context *ctx = arg;
 	printf("tid %d\n", ctx->tid);
+
 	while(1) {
 		struct buffer_descriptor* bd;
 		ring_get(ring, bd);
@@ -179,7 +190,9 @@ void *thread_function(void *arg) {
  * Prepares the context for each thread
 */
 void start_threads() {
+	printf("start of start threads\n");
     for (int i = 0; i < num_threads; i++) {
+    	printf("thread\n");
 	    contexts[i].tid = i;
         if (pthread_create(&threads[i], NULL, &thread_function, &contexts[i])) {
         	perror("pthread_create");
@@ -197,17 +210,123 @@ void wait_for_threads() {
 }
 
 int main(int argc, char *argv[]) {
-
+	printf("server\n");
 	if (parse_args(argc, argv) != 0) {
 	    exit(EXIT_FAILURE);
 	}
+	printf("parsed args\n");
+	printf("hash_size %d\n", hash_size);
 	// initialize locks for each hash index
 	for (int i = 0; i < hash_size; i++) {
 		pthread_mutex_init(&mutex[i], NULL);
 	}
-	
+	printf("initialized locks\n");
 	// TODO get shared memory... init_server();
 	init_server();
-
+	printf("inited server\n");
 	start_threads();
+	printf("started threads\n");
+	
+/*	
+	put(49, 2701209440);
+	put(45, 3602669057);
+	get(32);
+	put(37, 2495837118);
+	get(10);
+	get(20);
+	put(42, 2811417357);
+	put(21, 1727595483);
+    get(50);
+	get(39);
+	get(17);
+	put(31, 907538104);
+	get(9);
+	put(26, 635642472);
+	put(50, 1489802152);
+	get(27);
+	get(10);
+	put(9, 1168427025);
+	put(19, 2462587621);
+	get(15);
+	put(16, 1387294160);
+	get(41);
+	get(39);
+	put(33, 2589848910);
+	put(29, 2589600969);
+	get(17);
+	get(26);
+	get(8);
+	put(2, 2657429931);
+	put(48, 1533176625);
+	put(1, 1899449759);
+	put(46, 1502328340);
+	put(32, 1670425044);
+	get(21);
+	put(17, 1171968046);
+	get(44);
+	get(33);
+	get(28);
+	put(43, 2475444797);
+	put(4, 1380810609);
+	put(39, 1061995247);
+	put(20, 3506616455);
+	get(9);
+	put(7, 1848306733);
+	put(30, 2435745368);
+	put(10, 3284124238);
+	get(27);
+	get(49);
+	put(38, 840009267);
+	get(4);
+	get(36);
+	get(11);
+	get(20);
+	get(17);
+	get(22);
+	get(43);
+	put(44, 2442054922);
+	get(13);
+	get(23);
+	get(22);
+	get(26);
+	put(22, 1241709879);
+	get(23);
+	put(47, 641650486);
+	put(27, 1728112374);
+	get(36);
+	get(27);
+	put(8, 977265603);
+	get(18);
+	get(4);
+	put(24, 3957874317);
+	get(32);
+	get(36);
+	get(43);
+	put(36, 2927698958);
+	get(20);
+	get(17);
+	put(41, 1979428607);
+	get(9);
+	get(27);
+	put(6, 1530601361);
+	put(35, 2921342100);
+	put(23, 774606382);
+	get(37);
+	put(34, 1673128353);
+	put(18, 1643306410);
+	put(40, 658446124);
+	get(17);
+	put(13, 3291334975);
+	put(3, 264256110);
+	put(12, 1108079365);
+	put(14, 623815409);
+	get(36);
+	get(21);
+	put(5, 3186715834);
+	put(25, 741019578);
+	printf("%d\n", get(3));
+	put(15, 3268754311);
+	put(28, 2018964216);
+	put(11, 1434712982);
+	*/
 }
